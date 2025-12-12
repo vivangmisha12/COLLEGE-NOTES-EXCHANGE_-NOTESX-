f75665e4b807eabe6c7b9d28b601f426ba4c0c68
Vivang Mishra <your_email@example.com>
2025-12-12 15:57:24 +0530

    Fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: false,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Attach Token
