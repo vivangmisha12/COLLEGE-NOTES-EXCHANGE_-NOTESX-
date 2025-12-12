703c19dfd8d2b9e6093b7d19b3e3272bc3ae1ac1
Vivang Mishra <your_email@example.com>
2025-12-12 14:33:27 +0530

    Fix Code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,14 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,        // mysql.railway.internal
+  user: process.env.DB_USER,        // root
+  password: process.env.DB_PASSWORD,// railway password
+  database: process.env.DB_NAME,    // railway
+  port: process.env.DB_PORT,        // 3306
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool;
