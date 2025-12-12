acfecc56566fd2f3aa79045f38d2d867f85c61a4
Vivang Mishra <your_email@example.com>
2025-12-12 15:00:55 +0530

    Fix Code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // e.g. tramway.proxy.rlwy.net
-  port: process.env.DB_PORT,       // e.g. 22844
-  user: process.env.DB_USER,       // root
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
@@ -13,4 +13,4 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-export default pool;
+export default db;
